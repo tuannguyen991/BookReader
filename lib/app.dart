40ef02df7f62b9a0e8d7a66be5a411a62b2b5536
anhtuan <tuan.nguyenkieuanh@gmail.com>
2022-07-26 09:02:25 +0700

    d

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,6 @@ import 'package:demo_book_reader/theme/app_colors.dart';
 import 'package:demo_book_reader/theme/constant.dart';
 import 'package:flutter/material.dart';
 
-import 'features/home/author_list/author_list_page.dart';
 import 'features/onboarding/onboarding_page.dart';
 
 class MyApp extends StatelessWidget {
