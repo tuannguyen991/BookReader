9b3987feca81ce664719505cc54a34733c404c5a
Nguyễn Kiều Anh Tuấn <>
2022-07-26 02:04:42 +0000

    Updated app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,15 @@
+import 'package:demo_book_reader/features/onboarding/onboarding_page.dart';
 import 'package:demo_book_reader/theme/app_colors.dart';
 import 'package:demo_book_reader/theme/constant.dart';
 import 'package:flutter/material.dart';
 
-import 'features/home/author_list/author_list_page.dart';
-import 'features/onboarding/onboarding_page.dart';
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: titleApp,
       theme: ThemeData(
         primarySwatch: AppColors.primaryColor,
         scaffoldBackgroundColor: AppColors.backgroundColor,
