4af05fbaef6d11bf9058fd5f3ac5a3cea5f90361
hbngo21 <ngothihabac2772001@gmail.com>
2023-04-16 15:50:19 +0700

    Test payment with Zalopay

--- lib/main.dart
+++ lib/main.dart
@@ -1,253 +1,250 @@
-import 'package:demo_book_reader/di/locator.dart';
+import 'package:demo_book_reader/payment.dart';
+import 'package:demo_book_reader/theme_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_zalopay_sdk/flutter_zalopay_sdk.dart';
 
-import 'app.dart';
+import 'config.dart';
 
-Future<void> main() async {
-  setupLocator();
-  runApp(const MyApp());
+void main() => runApp(App());
 
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppConfig.appName,
+      theme: myTheme,
+      home: Dashboard(
+        title: AppConfig.appName,
+        version: AppConfig.version,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 
-// fvm flutter pub run build_runner build --delete-conflicting-outputs
 
-// import 'package:demo_book_reader/extensions/build_context_extensions.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:intl_phone_field/intl_phone_field.dart';
+class Dashboard extends StatefulWidget {
+  final String title;
+  final String version;
+  Dashboard({required this.title, required this.version});
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(const MyApp());
-// }
+  @override
+  _DashboardState createState() => _DashboardState();
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(
+          elevation: 0,
+          // backgroundColor: Colors.transparent,
+          leading: Center(
+            child: Text(widget.version),
+          ),
+          title: Text(
+            widget.title,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+        body: SafeArea(
+          child: HomeZaloPay(widget.title),
+        ),
+      ),
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+    );
+  }
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Firebase Phone Auth Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.red,
-//       ),
-//       home: const LoginPage(),
-//     );
-//   }
-// }
+class HomeZaloPay extends StatefulWidget {
+  final String title;
 
-// class LoginPage extends StatefulWidget {
-//   const LoginPage({super.key});
+  HomeZaloPay(this.title);
 
-//   @override
-//   // ignore: library_private_types_in_public_api
-//   _LoginPageState createState() => _LoginPageState();
-// }
+  @override
+  _HomeZaloPayState createState() => _HomeZaloPayState();
+}
 
-// class _LoginPageState extends State<LoginPage> {
-//   final _phoneNumberController = TextEditingController();
+class _HomeZaloPayState extends State<HomeZaloPay> {
+  final textStyle = TextStyle(color: Colors.black54);
+  final valueStyle = TextStyle(
+      color: AppColor.accentColor, fontSize: 18.0, fontWeight: FontWeight.w400);
+  String zpTransToken = "";
+  String payResult = "";
+  String payAmount = "10000";
+  bool showResult = false;
 
-//   bool _isLoading = false;
+  @override
+  void initState() {
+    super.initState();
+  }
 
-//   Future<void> _signInWithPhoneNumber(BuildContext context) async {
-//     try {
-//       // Show a loading spinner
-//       setState(() {
-//         _isLoading = true;
-//       });
+  // Button Create Order
+  Widget _btnCreateOrder(String value) => Padding(
+    padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+    child: GestureDetector(
+      onTap: () async {
+        int amount = int.parse(value);
+        if (amount < 1000 || amount > 1000000) {
+          setState(() {
+            zpTransToken = "Invalid Amount";
+          });
+        } else {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              });
+          var result = await createOrder(amount);
+          if (result != null) {
+            Navigator.pop(context);
+            zpTransToken = result.zptranstoken;
+            print("zpTransToken $zpTransToken'.");
+            setState(() {
+              zpTransToken = result.zptranstoken;
+              showResult = true;
+            });
+          }
+        }
+      },
+      child: Container(
+          height: 50.0,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: AppColor.primaryColor,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Text("Create Order",
+              style: TextStyle(color: Colors.white, fontSize: 20.0))),
+    ),
+  );
 
-//       // Start the phone number verification process
-//       verified(AuthCredential authResult) {}
+  /// Build Button Pay
+  Widget _btnPay(String zpToken) => Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+      child: Visibility(
+        visible: showResult,
+        child: GestureDetector(
+          onTap: () async {
+            FlutterZaloPaySdk.payOrder(zpToken: zpToken).listen((event) {
+              setState(() {
+                switch (event) {
+                  case FlutterZaloPayStatus.cancelled:
+                    payResult = "User Huỷ Thanh Toán";
+                    break;
+                  case FlutterZaloPayStatus.success:
+                    payResult = "Thanh toán thành công";
+                    break;
+                  case FlutterZaloPayStatus.failed:
+                    payResult = "Thanh toán thất bại";
+                    break;
+                  default:
+                    payResult = "Thanh toán thất bại";
+                    break;
+                }
+              });
+            });
+          },
+          child: Container(
+              height: 50.0,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: AppColor.primaryColor,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text("Pay",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0))),
+        ),
+      ));
 
-//       verificationFailed(FirebaseAuthException authException) {
-//         // Handle phone verification failure
-//         setState(() {
-//           _isLoading = false;
-//         });
-//         if (authException.code == 'invalid-phone-number') {
-//           showDialog(
-//             context: context,
-//             builder: (context) {
-//               return AlertDialog(
-//                   title: const Text('Lỗi xác thực số điện thoại'),
-//                   content: const Text('Mời nhập lại số điện thoại'),
-//                   actions: [
-//                     TextButton(
-//                       onPressed: () {
-//                         context.off();
-//                       },
-//                       child: const Text('OK'),
-//                     ),
-//                   ]);
-//             },
-//           );
-//         }
-//         // ScaffoldMessenger.of(context).showSnackBar(
-//         //   SnackBar(
-//         //     content: Text('Error: ${authException.message}'),
-//         //   ),
-//         // );
-//       }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          _quickConfig,
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Amount',
+              icon: Icon(Icons.attach_money),
+            ),
+            initialValue: payAmount,
+            onChanged: (value) {
+              setState(() {
+                payAmount = value;
+              });
+            },
+            keyboardType: TextInputType.number,
+          ),
+          _btnCreateOrder(payAmount),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: Visibility(
+              child: Text(
+                "zptranstoken:",
+                style: textStyle,
+              ),
+              visible: showResult,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: Text(
+              zpTransToken,
+              style: valueStyle,
+            ),
+          ),
+          _btnPay(zpTransToken),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: Visibility(
+                child: Text("Transaction status:", style: textStyle),
+                visible: showResult),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: Text(
+              payResult,
+              style: valueStyle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-//       smsSent(String verificationId, [int? forceResendingToken]) async {
-//         // Navigate to the VerifyCodeScreen widget
-//         final String returnedValue = await Navigator.push(
-//           context,
-//           MaterialPageRoute(
-//             builder: (context) => VerifyCodeScreen(
-//               verificationId: verificationId,
-//               text: _phoneNumberController.text,
-//             ),
-//           ),
-//         );
+/// Build Info App
+Widget _quickConfig = Container(
+  margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  child: Row(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Text("AppID: 2553"),
+          ),
+        ],
+      ),
+      // _btnQuickEdit,
+    ],
+  ),
+);
 
-//         setState(() {
-//           _isLoading = false;
-//           _phoneNumberController.text = returnedValue;
-//         });
-//       }
-
-//       autoTimeout(String verificationId) {}
-
-//       await FirebaseAuth.instance.verifyPhoneNumber(
-//         phoneNumber: _phoneNumberController.text,
-//         verificationCompleted: verified,
-//         verificationFailed: verificationFailed,
-//         codeSent: smsSent,
-//         codeAutoRetrievalTimeout: autoTimeout,
-        
-//       );
-//     } catch (e) {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(
-//           content: Text('Error: $e'),
-//         ),
-//       );
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Firebase Phone Auth Demo'),
-//       ),
-//       body: _isLoading
-//           ? const Center(child: CircularProgressIndicator())
-//           : Padding(
-//               padding: const EdgeInsets.all(16),
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   IntlPhoneField(
-//                     decoration: const InputDecoration(
-//                       labelText: 'Phone Number',
-//                     ),
-//                     initialCountryCode: 'VN',
-//                     initialValue: _phoneNumberController.text.replaceFirst('+84', '0'),
-//                     onChanged: (phone) {
-//                       phone.number = phone.number.replaceFirst('0', '');
-//                       _phoneNumberController.text = phone.completeNumber;
-//                     },
-//                     disableLengthCheck: true,
-//                   ),
-//                   const SizedBox(height: 48),
-//                   ElevatedButton(
-//                     onPressed: () => _signInWithPhoneNumber(context),
-//                     child: const Text('Verify Phone Number'),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//     );
-//   }
-// }
-
-// class VerifyCodeScreen extends StatefulWidget {
-//   final String verificationId;
-//   final String text;
-
-//   const VerifyCodeScreen(
-//       {super.key, required this.verificationId, required this.text});
-
-//   @override
-//   // ignore: library_private_types_in_public_api
-//   _VerifyCodeScreenState createState() => _VerifyCodeScreenState();
-// }
-
-// class _VerifyCodeScreenState extends State<VerifyCodeScreen> {
-//   final _smsCodeController = TextEditingController();
-//   // final _phoneController = TextEditingController(text: widget.text);
-
-//   Future<void> _verifySMSCode(BuildContext context) async {
-//     try {
-//       // Create a PhoneAuthCredential with the SMS code
-//       var credential = PhoneAuthProvider.credential(
-//         verificationId: widget.verificationId,
-//         smsCode: _smsCodeController.text,
-//       );
-
-//       // Sign the user in with the credential
-//       final authResult =
-//           await FirebaseAuth.instance.signInWithCredential(credential);
-
-//       if (authResult.user != null) {
-//         // ignore: use_build_context_synchronously
-//         ScaffoldMessenger.of(context).showSnackBar(
-//           const SnackBar(
-//             content: Text('Sign in successfully'),
-//           ),
-//         );
-//       } else {
-//         // ignore: use_build_context_synchronously
-//         ScaffoldMessenger.of(context).showSnackBar(
-//           const SnackBar(
-//             content: Text('Sign in failed'),
-//           ),
-//         );
-//       }
-//     } catch (e) {
-//       // Handle sign in failure
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(
-//           content: Text('Error: $e'),
-//         ),
-//       );
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         leading: BackButton(
-//           onPressed: () => Navigator.pop(context, widget.text),
-//         ),
-//         title: const Text('Enter SMS Code'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             TextField(
-//               controller: _smsCodeController,
-//               decoration: const InputDecoration(
-//                 labelText: 'SMS Code',
-//               ),
-//               keyboardType: TextInputType.number,
-//             ),
-//             // ignore: prefer_const_constructors
-//             SizedBox(height: 16),
-//             ElevatedButton(
-//               onPressed: () => _verifySMSCode(context),
-//               child: const Text('Verify SMS Code'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
